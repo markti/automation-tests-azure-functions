57ef492cf57ffcd83589c47d9255dda48e35d37c
markti <markti@outlook.com>
2025-07-02 11:50:19 -0400

    sensitive

--- testing/deploy-azure-fn/outputs.tf
+++ testing/deploy-azure-fn/outputs.tf
@@ -1,3 +1,4 @@
 output "function_key" {
-  value = data.azurerm_function_app_host_keys.main.default_function_key
+  value     = data.azurerm_function_app_host_keys.main.default_function_key
+  sensitive = true
 }
