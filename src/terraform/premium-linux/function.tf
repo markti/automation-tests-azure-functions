8791de7b47719990f921a87df84247dcfdd2e46a
markti <markti@outlook.com>
2025-07-03 12:19:58 -0400

    premium-linux

--- src/terraform/premium-linux/function.tf
+++ src/terraform/premium-linux/function.tf
@@ -4,6 +4,23 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "azurerm_user_assigned_identity" "function" {
+
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  name                = "mi-${var.application_name}-${var.environment_name}"
+
+}
+
+# Grants the Azure Function Reader access to the Key Vault
+resource "azurerm_role_assignment" "function_keyvault_reader" {
+
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.function.principal_id
+
+}
+
 resource "azurerm_service_plan" "main" {
   name                = "asp-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.main.name
@@ -13,20 +30,71 @@ resource "azurerm_service_plan" "main" {
 }
 
 resource "azurerm_storage_account" "function" {
-  name                     = "st${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = "st${random_string.suffix.result}"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  shared_access_key_enabled       = false
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = true # My GitHub Runners need to use the internet to access the storage account
+}
+
+
+# Allow User Assigned Identity Access to Function Operational Storage Account
+resource "azurerm_role_assignment" "function_user_assigned_storage_contributor" {
+
+  scope                = azurerm_storage_account.function.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_user_assigned_identity.function.principal_id
+
+}
+
+# Allow User Assigned Identity Access to Function Operational Storage Account Blob Storage
+resource "azurerm_role_assignment" "function_user_assigned_storage_blob_data_owner" {
+
+  scope                = azurerm_storage_account.function.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_user_assigned_identity.function.principal_id
+
+}
+
+# Allow System Assigned Identity Access to Function Operational Storage Account
+resource "azurerm_role_assignment" "function_system_assigned_storage_contributor" {
+
+  scope                = azurerm_storage_account.function.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+
+}
+
+# Allow System Assigned Identity Access to Function Operational Storage Account Blob Storage
+resource "azurerm_role_assignment" "function_system_assigned_storage_blob_data_owner" {
+
+  scope                = azurerm_storage_account.function.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+
 }
 
 resource "azurerm_linux_function_app" "main" {
-  name                       = "func-dotnet-${var.application_name}-${var.environment_name}-${random_string.suffix.result}"
-  resource_group_name        = azurerm_resource_group.main.name
-  location                   = azurerm_resource_group.main.location
-  service_plan_id            = azurerm_service_plan.main.id
-  storage_account_name       = azurerm_storage_account.function.name
-  storage_account_access_key = azurerm_storage_account.function.primary_access_key
+  name                                           = "func-dotnet-${var.application_name}-${var.environment_name}-${random_string.suffix.result}"
+  resource_group_name                            = azurerm_resource_group.main.name
+  location                                       = azurerm_resource_group.main.location
+  service_plan_id                                = azurerm_service_plan.main.id
+  key_vault_reference_identity_id                = azurerm_user_assigned_identity.function.id
+  storage_account_name                           = azurerm_storage_account.function.name
+  storage_uses_managed_identity                  = true
+  ftp_publish_basic_authentication_enabled       = false
+  https_only                                     = true
+  webdeploy_publish_basic_authentication_enabled = false
+
+  identity {
+    type = "SystemAssigned, UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.function.id
+    ]
+  }
 
   site_config {
     use_32_bit_worker = false
---
+++ src/terraform/premium-linux/keyvault.tf
@@ -0,0 +1,19 @@
+resource "azurerm_key_vault" "main" {
+
+  name                        = "kv-${random_string.suffix.result}"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  enable_rbac_authorization   = true
+  purge_protection_enabled    = true
+
+}
+
+# Grants Terraform Administrator access to the Key Vault
+resource "azurerm_role_assignment" "terraform_user_keyvault_admin" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.client_id
+}
